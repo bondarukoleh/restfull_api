c5143436efbb2a6f508374822c0ffadab5de832d
Oleh Bondaruk <bondaruk9000@gmail.com>
2020-01-27 00:32:29 +0200

    adding test examples for jest

--- src/test/jest.config.js
+++ src/test/jest.config.js
@@ -112,9 +112,9 @@ module.exports = {
   rootDir: '../test',
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    '../../info/some_code/jest'
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
--- src/test/unit/unit.test.js
+++
@@ -1,9 +0,0 @@
-
-function add(a, b) {
-	return a + b;
-}
-
-test('Some simple test', () => {
-	expect(add(1, 2)).toBe(3)
-});
-
