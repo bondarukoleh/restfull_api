5d1d9e14540b446f18782a6ddff42f9ca64aff86
Oleh Bondaruk <bondaruk9000@gmail.com>
2020-02-04 21:35:39 +0200

    prepearing for deployment

--- src/startup/middleware.js
+++ src/startup/middleware.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
+const compression = require('compression');
 
 module.exports = function(app) {
 	app.use(express.json()); // for application/json
 	app.use(express.urlencoded({extended: true})); // for application/x-www-form-urlencoded
 	app.use(express.static('./src/public')); // static serving from public folder
 	app.use(helmet()); // increases security
+	app.use(compression()); // increases security
 	app.set('view engine', 'pug');
 	app.set('views', './src/views'); // views set by default - here for example
 };
